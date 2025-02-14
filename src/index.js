158b06363912f6e481998dfa44070c78a5825c77
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-14 17:54:02 +0100

    adding hardcoded tasks after clicking on New task button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import {project} from './project.js';
+import {task} from './task.js';
 
 export const body = document.querySelector("body");
 
@@ -7,6 +8,8 @@ export const body = document.querySelector("body");
 const projects_container = document.querySelector("div.projects_container");
 
 const projectt = document.querySelector("button.add_project");
+const taskss = document.querySelector("button.add_task");
+
 
 (function add_project(){
 
@@ -15,3 +18,12 @@ projectt.addEventListener('click', (event)=>
         project(); 
     }); }
 )();
+
+
+(function add_tasks(){
+
+    taskss.addEventListener('click', (event)=>
+        {
+            task(); 
+        }); }
+    )();
---
+++ src/task.js
@@ -0,0 +1,11 @@
+
+import { body } from "./index.js";
+
+
+export function task() {
+    const contentElement = document.querySelector(".tasks_div");
+    const newtask = document.createElement("p");
+    newtask.textContent = "task1"
+    contentElement.appendChild(newtask);
+    return contentElement;
+}
