8fe59818ebb4cc5127085800a5d847f45334ceb1
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-14 17:43:49 +0100

    add new project each time the new project button is being clicked

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const projects_container = document.querySelector("div.projects_container");
 
 const projectt = document.querySelector("button.add_project");
 
-
+(function add_project(){
 
 projectt.addEventListener('click', (event)=>
     {
         project(); 
-    }); 
+    }); }
+)();
