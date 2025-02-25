d6c0149771cffba31a54ce0180ddbe33c2a573b0
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-25 16:08:36 +0100

    appliying the prettier formatter on ui.js and projects.js

--- src/project.js
+++ src/project.js
@@ -1,69 +1,62 @@
-
-import {togglePopup, togglePopup1} from './ui.js';
-import { Task } from './task.js';
-
+import { Task } from "./task.js";
 
 export class ProjectManager {
-    constructor() {
-        this.projects = [];
-    }
-    addProject(name) {
-        var newProject = new Project(name);
-        this.projects.push(newProject);
-        return newProject;
-    }
+  constructor() {
+    this.projects = [];
+  }
+  addProject(name) {
+    var newProject = new Project(name);
+    this.projects.push(newProject);
+    return newProject;
+  }
 
-    deleteProject(name) {
-        this.projects.splice(this.projects.findIndex(v => v.name === name), 1);
-    }
+  deleteProject(name) {
+    this.projects.splice(
+      this.projects.findIndex((v) => v.name === name),
+      1,
+    );
+  }
 
-    editProject(newName, oldName) {
-        var project = this.projects.find((project) => project.name === oldName);
-        project.name = newName;
-    }
+  editProject(newName, oldName) {
+    var project = this.projects.find((project) => project.name === oldName);
+    project.name = newName;
+  }
 
-    getProjects(){
-        return this.projects;
-    }
+  getProjects() {
+    return this.projects;
+  }
 
-    getProjectByName(name){
-        return this.projects.find((project) => project.name === name);
-    }
-    //edit_project .....
-
+  getProjectByName(name) {
+    return this.projects.find((project) => project.name === name);
+  }
+  //edit_project .....
 }
 
 export class Project {
-    constructor(name){
-        this.name = name;
-        this.tasks = [];
-    }
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
 
-    addTask(name, priority, description){
-        var task = new Task(name, priority, description);
-        this.tasks.push(task);
-        return task;
-    }
+  addTask(name, priority, description) {
+    var task = new Task(name, priority, description);
+    this.tasks.push(task);
+    return task;
+  }
 
-    removeTask(name){
-        this.tasks = this.tasks.filter ( item => item.name!==name);
-    }
+  removeTask(name) {
+    this.tasks = this.tasks.filter((item) => item.name !== name);
+  }
 
-    removeAllTasks(){
-        this.tasks = [];
-    }
+  removeAllTasks() {
+    this.tasks = [];
+  }
 
-    getTasks(){
-        return this.tasks;
-    }
+  getTasks() {
+    return this.tasks;
+  }
 
-    getTaskByName(name){
-        return this.tasks.find((task) => task.name === name);
-    }
-
+  getTaskByName(name) {
+    return this.tasks.find((task) => task.name === name);
+  }
 }
-
-
-
-  
-    
